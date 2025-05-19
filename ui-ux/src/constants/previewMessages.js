4db46b8c95c44ed2c6f558c448000a420b65919b
Hanz Chiziterem <hancarta23@gmail.com>
2025-05-19 11:52:00 +0100

    Some utils needed for theme selection and previews.

---
+++ ui-ux/src/constants/previewMessages.js
@@ -0,0 +1,6 @@
+const PREVIEW_MESSAGES = [
+    {id: 1, content: "Hey, How is it going?", isSent: false},
+    {id: 2, content: "I'm doing great! just working on some new features.", isSent: true},
+];
+
+export default PREVIEW_MESSAGES;
