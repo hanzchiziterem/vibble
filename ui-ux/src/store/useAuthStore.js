8a359d85d99f8289669d6c0e840a09a2971d6258
Hanz Chiziterem <hancarta23@gmail.com>
2025-05-19 11:02:01 +0100

    Implement the register user functionality.

--- ui-ux/src/store/useAuthStore.js
+++ ui-ux/src/store/useAuthStore.js
@@ -10,6 +10,34 @@ export const useAuthStore = create((set) => ({
 
   isCheckingAuth: true,
 
+  checkAuth: async () => {
+    try {
+      const res = await axiosInstance.get("/auth/check");
+      set({ authUser: res.data });
+    } catch (error) {
+      console.log("Error in checkAuth: ", error);
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
+
+  signup: async (data) => {
+    set({ isSigningUp: true });
+    try {
+      const res = await axiosInstance.post("/auth/signup", data);
+
+      set({ authUser: res.data });
+      toast.success("Account created successfully.");
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({
+        isSigningUp: false,
+      });
+    }
+  },
+
   updateProfile: async (data) => {
     set({ isUpdatingProfile: true });
     try {
